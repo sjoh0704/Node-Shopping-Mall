fe46cd85bff0f4bcfcd165d6216d12fc76a46d0b
seungjuoh <sjoh0704@daum.net>
2021-07-31 13:22:33 +0900

    add post user api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/user")
 
 mongoose.connect("mongodb://172.17.0.3/shopping-demo", {
   useNewUrlParser: true,
@@ -10,6 +11,31 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 const app = express();
 const router = express.Router();
+
+router.post('/users', async(req, res)=>{
+    const {nickname, email, password, confirmPassword} = req.body;
+    if(password!==confirmPassword){
+        res.status(400).send({
+            errorMessage: "비밀번호가 다르네요"
+        });
+        return;
+    }
+    const existUser = await User.find({
+        $or:[{email}, {nickname}]
+    });
+    if(existUser.length){
+        res.status(400).send({
+            errorMessage: "닉네임이나 이메일이 존재합니다."
+        });
+        return;
+    }
+    
+    const user = new User({email, nickname, password});
+    await user.save();
+    res.send({message: "success"});
+});
+
+
 
 app.use("/api", express.urlencoded({ extended: false }), router);
 app.use(express.static("assets"));
