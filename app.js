23c0e2b953111203ea139eac2abb5e9b64305024
seungjuoh <sjoh0704@daum.net>
2021-07-31 12:05:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://172.17.0.3/shopping-demo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+const app = express();
+const router = express.Router();
+
+app.use("/api", express.urlencoded({ extended: false }), router);
+app.use(express.static("assets"));
+
+app.listen(8080, () => {
+  console.log("서버가 요청을 받을 준비가 됐어요");
+});
