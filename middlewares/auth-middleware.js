beaaa0e9406a223429a4e224d78e891d17640c01
seungjuoh <sjoh0704@daum.net>
2021-08-10 17:51:55 +0900

    add mysql database

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user')
+const User = require('../models/user_mongo')
 module.exports = (req, res, next) => {
 
     // 여기서 토큰을 검사한다. 
--- router/user.js
+++ router/user.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const router = express.Router();
-const User = require("../models/user")
+const User = require("../models/user_mongo")
 const jwt = require('jsonwebtoken');
 const authMiddleware = require('../middlewares/auth-middleware')
 
